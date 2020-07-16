1c494cb33ca379d545095620fc5139c73f9809d4
Patrick Sheehan <psheehan287@gmail.com>
2020-07-16 13:49:37 -0500

    Don't specify '-fopenmp' in setup.py by default, as this doesn't work by default on Macs.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ import numpy
 trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
         include_dirs=[numpy.get_include(),"./include",\
         "./delaunator-cpp/include"], language="c++", \
-        extra_compile_args=['-std=c++11','-Ofast','-march=native','-fopenmp'],\
-        extra_link_args=["-std=c++11",'-Ofast','-march=native','-fopenmp'])
+        extra_compile_args=['-std=c++11','-Ofast','-march=native'],\
+        extra_link_args=["-std=c++11",'-Ofast','-march=native'])
 
 setup(name="trift", version="0.9.0", packages=["trift"], \
         cmdclass={'build_ext':build_ext}, ext_modules=[trift])
