661bcd927bc6e2598eb335f939917116034ee15c
Patrick Sheehan <psheehan287@gmail.com>
2020-07-16 16:46:04 -0500

    Try to compile with OpenMP, and if that fails, compile without.

--- setup.py
+++ setup.py
@@ -5,11 +5,22 @@ import os
 
 import numpy
 
-trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
-        include_dirs=[numpy.get_include(),"./include",\
-        "./delaunator-cpp/include"], language="c++", \
-        extra_compile_args=['-std=c++11','-Ofast','-march=native'],\
-        extra_link_args=["-std=c++11",'-Ofast','-march=native'])
+try:
+    trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
+            include_dirs=[numpy.get_include(),"./include",\
+            "./delaunator-cpp/include"], language="c++", \
+            extra_compile_args=['-std=c++11','-Ofast','-march=native',\
+            '-fopenmp'],extra_link_args=["-std=c++11",'-Ofast','-march=native',\
+            '-fopenmp'])
 
-setup(name="trift", version="0.9.0", packages=["trift"], \
-        cmdclass={'build_ext':build_ext}, ext_modules=[trift])
+    setup(name="trift", version="0.9.0", packages=["trift"], \
+            cmdclass={'build_ext':build_ext}, ext_modules=[trift])
+except:
+    trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
+            include_dirs=[numpy.get_include(),"./include",\
+            "./delaunator-cpp/include"], language="c++", \
+            extra_compile_args=['-std=c++11','-Ofast','-march=native'],\
+            extra_link_args=["-std=c++11",'-Ofast','-march=native'])
+
+    setup(name="trift", version="0.9.0", packages=["trift"], \
+            cmdclass={'build_ext':build_ext}, ext_modules=[trift])
